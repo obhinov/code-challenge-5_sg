eb5e63533267f6cec756147912e8308b892af40d
Abhinav Subramani <abhisubramani3@gmail.com>
2022-10-31 17:52:34 -0400

    added delete.js file

---
+++ delete.js
@@ -0,0 +1,59 @@
+const aws = require('aws-sdk');
+
+const dynamodb = new aws.DynamoDB; // get the dynamoDB SDK
+
+const dynamoTable = process.env.TABLE_NAME;
+
+const api_function = {};
+
+api_function.handler = async(event) => {
+    console.log(event);
+
+    let response = {};
+
+    try {
+        if (event.path=='/users_delete' && event.httpMethod=='DELETE'){
+            response.body = JSON.stringify(await api_function.deleteUser(JSON.parse(event.body)));
+        }
+        response.statusCode = 200;
+    } catch(e) {
+        response.body = JSON.stringify(e);
+        response.statusCode = 500;
+    }
+    console.log(response);
+    return response;
+
+};
+
+api_function.deleteUser = (item) => {
+    return new Promise((resolve,reject) => {
+        let params = {
+            TableName: dynamoTable,
+            Key: {
+                "user_id": {
+                    S: item.user_id.S
+                }
+            }
+        };
+
+        dynamodb.deleteItem(params, (err,data) => {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
+};
+
+/*
+event format:
+- event.httpMethod = 'POST'
+- event.path = '/users'
+- event.body = '{\n    "user_id": "BWMP-883",\n    "name": "De Niro"\n}'
+*/
+
+/*
+API Request Body format:
+{"user_id": {"S": "baba43"}, "name": {"S": "thomas"}}
+*/
+
+module.exports = api_function;
+// create
